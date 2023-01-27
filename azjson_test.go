00517f08c8c0a3fa253af20e6f980afa9c4efef2
Stian Frøystein <stianfroy@gmail.com>
2023-01-27 15:04:09 +0900

    test(azjson): add test for post function

--- azjson_test.go
+++ azjson_test.go
@@ -1,6 +1,7 @@
 package azjson
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,7 +14,10 @@ func TestGet(t *testing.T) {
 	// Create a test server to mock the API response
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`{"key": "value"}`))
+		_, err := w.Write([]byte(`{"key": "value"}`))
+		if err != nil {
+			t.Error(err)
+		}
 	}))
 	defer ts.Close()
 
@@ -26,4 +30,52 @@ func TestGet(t *testing.T) {
 	// Assert that the json response and error are as expected
 	assert.Equal(t, `{"key": "value"}`, json)
 	assert.NoError(t, err)
+}
+
+func TestPost(t *testing.T) {
+	url := "http://localhost:8080"
+	payload := []byte(`{"example": "payload"}`)
+	token := azcore.AccessToken{Token: "test-token"}
+	id := "123"
+
+	// Start a test server
+	server := &http.Server{Addr: ":8080"}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Error("Expected POST method, got:", r.Method)
+		}
+
+		// Verify that the request headers match the expected values
+		if r.Header.Get("Authorization") != "Bearer "+token.Token {
+			t.Error("Expected authorization header to be 'Bearer "+token.Token+"', got:", r.Header.Get("Authorization"))
+		}
+		if r.Header.Get("Content-Type") != "application/json" {
+			t.Error("Expected content-type header to be 'application/json', got:", r.Header.Get("Content-Type"))
+		}
+
+		// Verify that the request body matches the expected payload
+		var reqPayload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&reqPayload)
+		if err != nil {
+			t.Error("Error decoding request body:", err)
+		}
+		if reqPayload["example"] != "payload" {
+			t.Error("Expected payload to be `{\"example\": \"payload\"}`, got:", reqPayload)
+		}
+	})
+
+	// Check the error return value of Listen and Serve
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			t.Error("Error starting server:", err)
+		}
+		defer server.Close()
+	}()
+
+	// Test success case
+	err := Post(url, payload, token, id)
+	if err != nil {
+		t.Error("Expected nil error, got:", err)
+	}
 }
